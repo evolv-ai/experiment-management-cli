08249affaca96ce350dcc93646616f8e6ab14518
Frazer Bayley <bayley.frazer@gmail.com>
2019-07-22 15:23:47 -0700

    Logging message on login error

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='evolvcli',
-    version='1.0.0',
+    version='1.0.1',
     packages=find_packages(),
     include_package_data=True,
     license='Apache License 2.0',
